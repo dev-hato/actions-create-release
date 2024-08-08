e033a87d058ae2f44d263be4dc94a60d19d9ba9b
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-08-08 19:53:49 +0900

    super-linterアップデート (#1526)
    * Update super-linter/super-linter action to v6.8.0
    
    * super-linterによる指摘事項修正
    
    * prettierを使う
    
    * gitleaksをアップデートしてあげたよ！ (#1527)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/action/create_release.js
+++ scripts/action/create_release.js
@@ -4,8 +4,8 @@ module.exports = async ({ github, context }) => {
     repo: context.repo.repo,
     tag_name: process.env.TAG_NAME,
     target_commitish: process.env.GITHUB_REF,
-    generate_release_notes: true
-  }
-  console.log('call repos.createRelease:', createReleaseParams)
-  await github.rest.repos.createRelease(createReleaseParams)
-}
+    generate_release_notes: true,
+  };
+  console.log("call repos.createRelease:", createReleaseParams);
+  await github.rest.repos.createRelease(createReleaseParams);
+};

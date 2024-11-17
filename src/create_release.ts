60d813063788a7c63db0c8abe93fe3e9f751ea5d
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-11-17 21:32:02 +0900

    CIで使用しているスクリプトをTypeScript化する

--- src/create_release.ts
+++ src/create_release.ts
@@ -1,11 +1,25 @@
-module.exports = async ({ github, context }) => {
-  const createReleaseParams = {
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    tag_name: process.env.TAG_NAME,
-    target_commitish: process.env.GITHUB_REF,
-    generate_release_notes: true,
-  };
+import type { Context } from "@actions/github/lib/context";
+import type { GitHub } from "@actions/github/lib/utils";
+import type { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
+
+export async function script(
+  github: InstanceType<typeof GitHub>,
+  context: Context,
+) {
+  const tag_name = process.env.TAG_NAME;
+
+  if (tag_name === undefined) {
+    throw new Error("TAG_NAME must set.");
+  }
+
+  const createReleaseParams: RestEndpointMethodTypes["repos"]["createRelease"]["parameters"] =
+    {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      tag_name,
+      target_commitish: process.env.GITHUB_REF,
+      generate_release_notes: true,
+    };
   console.log("call repos.createRelease:", createReleaseParams);
   await github.rest.repos.createRelease(createReleaseParams);
-};
+}
